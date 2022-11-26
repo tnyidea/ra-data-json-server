04d671a4c47a19604c357dc58eba7e38c4572d03
gbnyc26 <gregbehrend26@gmail.com>
2022-11-25 23:59:19 -0500

    Added GetMany

--- go/service/addressv1/getone.go
+++ go/service/addressv1/getone.go
@@ -1,7 +1,13 @@
 package addressv1
 
+import (
+    "github.com/gin-gonic/gin"
+    datatypes "github.com/tnyidea/ra-data-json-server/go/data/types"
+    "net/http"
+)
+
 func GetOne(c *gin.Context) {
-    // GET http://my.api.url/posts/123
-    var address datatypes.Address
-    c.IndentedJSON(http.StatusOK, address)
+	// GET http://my.api.url/posts/123`
+	var address datatypes.Address
+	c.IndentedJSON(http.StatusOK, address)
 }
